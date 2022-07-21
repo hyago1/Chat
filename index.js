e9283b61ba38923d68e37ada4a069771f56df518
Hyago Philip <36174848+hyago1@users.noreply.github.com>
2022-07-20 23:55:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const { on } = require('events');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const bodyParser = require('body-parser');
+const { emit } = require('process');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+let s;
+let users = []
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/createRoom.html');
+});
+
+app.post('/chatRoom', (req, res) => {
+    s = { roomName, userName } = req.body;
+
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket, res) => {
+   const user = { id: socket.id, user: s.userName }
+    users.push(user)
+    
+    console.log('a user connected');
+    socket.join(s.roomName)
+    console.log(s.userName + " -- Entrou na sala: " + s.roomName);
+    socket.on('chat message', (msg) => {
+        let userChat;
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].id == socket.id ) {
+                userChat = users[i].user
+            }
+        } 
+       
+        io.to(roomName).emit('addMessage', msg, userChat)
+    })
+
+
+
+});
+
+
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ main.js
@@ -0,0 +1,11 @@
+// var socket = io();
+// let form = document.getElementById('form');
+// var inputRoom = document.getElementById('newRoomName');
+
+// form.addEventListener('submit', function(e) {
+//   e.preventDefault();
+//     if (inputRoom.value) {
+//       socket.emit('chat message', '/index.html',inputRoom.value ,"Create");
+
+//     }
+//   });
